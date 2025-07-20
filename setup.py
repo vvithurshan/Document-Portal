ee6b23f112b4caae8619010496ff109af151e116
vvithurshan <vvithurshan@gmail.com>
2025-07-20 10:08:41 -0600

    Notebook Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='document_portal',
+    author='Vithurshan',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/vvithurshan/Document-Portal',
+    description='Document Portal',
+    )
+
